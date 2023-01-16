f9743b31b4035f346d7a30fbab73751f0deb27a9
Louise MERY <lmery@e2r9p9.clusters.42paris.fr>
2023-01-16 21:29:24 +0100

    Debut de parser avec cmd->word et cmd->redir, fonctionnel

--- parser/parser.c
+++ parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: lmery <lmery@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 18:20:24 by lmery             #+#    #+#             */
-/*   Updated: 2023/01/16 19:06:12 by lmery            ###   ########.fr       */
+/*   Updated: 2023/01/16 21:27:49 by lmery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,36 @@ unset
 exit[___]
 */
 
+//A modifier pour etre utiliser dans le parser et associer les cmd
+t_list	*lst_add_token(t_list *lst_token, void *content)
+{
+	t_list	*new;
+
+	new = ft_lstnew(content);
+	if (new == NULL)
+		return (NULL);
+	ft_lstadd_back(&lst_token, new);
+	return (lst_token);
+}
+
 t_command	*parser(t_list *lst_token)
 {
 	t_token	*token;
+	t_token	*token_next;
 	t_command *command;
+	t_bool	heredoc_before;
+	// t_list	*lst_command;
 	int	i;
 	int	j;
 
 	j = 0;
 	i = 0;
+	// lst_command = NULL;
 	command = malloc(sizeof(t_command));
 	if (!command)
 		return (0);
-	command->cmd = malloc(sizeof(char *) * ft_lstsize(lst_token) + 1);
-	if (! command->cmd)
+	command->word = malloc(sizeof(char *) * ft_lstsize(lst_token) + 1);
+	if (! command->word)
 		return (0);
 	command->redir = malloc(sizeof(char *) * ft_lstsize(lst_token) + 1);
 	if (! command->redir)
@@ -41,20 +57,37 @@ t_command	*parser(t_list *lst_token)
 	while (lst_token)
 	{
 		token = lst_token->content;
-		if (token->type == TOKEN_WORD)
+		if (lst_token->next)
+		 	token_next = lst_token->next->content;
+		if (token->type == TOKEN_REDIRECT_IN || token->type == TOKEN_REDIRECT_OUT || \
+		(token_next != NULL && token->type == TOKEN_WORD && token_next->type == TOKEN_REDIRECT_OUT) || \
+		(token->type == TOKEN_WORD && heredoc_before == true))
 		{
-			command->cmd[i] = ft_strdup(token->text);
-			i++;
-		}
-		if (token->type == TOKEN_REDIRECT_IN || token->type == TOKEN_REDIRECT_OUT)
-		{
+			printf("test\n");
 			command->redir[j] = ft_strdup(token->text);
+			if (token->type == TOKEN_REDIRECT_IN)
+				heredoc_before = true;
+			//Ajouter ici tous les words en cours et la redir dans un maillon lst_cmd
 			j++;
+		}
+		else if (token->type == TOKEN_WORD)
+		{
+			command->word[i] = ft_strdup(token->text);
+			heredoc_before = false;
+			i++;
 		}
 		lst_token = lst_token->next;
 	}
-	command->cmd[i] = NULL;
+	command->word[i] = NULL;
 	command->redir[j] = NULL;
-	//printf (_BOLD "TEST = %s\n"_END, command->cmd[0]);
+	i = 0;
+	j = 0;
+	while (command->word[i] || command->redir [j])
+	{
+		if (command->word[i++])
+			printf (_BOLD "Word %d = %s\n"_END, i, command->word[i - 1]);
+		if (command->redir[j++])
+			printf (_BOLD "Redir %d = %s\n"_END, i, command->redir[j - 1]);
+	}
 	return (command);
 }
