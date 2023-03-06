21d68e39114e1712bd89ec24f114b44b59086444
Louise MERY <lmery@e2r1p12.clusters.42paris.fr>
2023-03-06 18:54:22 +0100

    fix exit without argument

--- exec/builtin_exit.c
+++ exec/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: lmery <lmery@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 04:20:53 by lmery             #+#    #+#             */
-/*   Updated: 2023/03/05 22:32:13 by lmery            ###   ########.fr       */
+/*   Updated: 2023/03/06 18:46:12 by lmery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ void	builtin_exit(char ***env, t_list *lst_command, char **cmd)
 	
 	i = 0;
 	j = 0;
+
 	if (cmd[1])
 	{
-
 		if ((!ft_isdigit(cmd[1][1]) || (!ft_isdigit(cmd[1][0]) && cmd[1][0] != '-' && cmd[1][0] != '+'))) 
 			res = 2;
 		else if (ft_atoi(cmd[1]) > 255)
@@ -50,16 +50,14 @@ void	builtin_exit(char ***env, t_list *lst_command, char **cmd)
 		}
 		if (j > 11)
 			res = 101;
-		// printf ("j = %d\n", j);
 		g_status = res;
 	}
-	if ((cmd[1][0] == '0'))
+	else
 		g_status = 0;
-	if (ft_maplen_secure(cmd) > 2 || ((cmd[1][0] == '1' && !cmd[1][1])))
+	if (ft_maplen_secure(cmd) > 2 || (cmd[1] && (cmd[1][0] == '1' && !cmd[1][1])))
 		g_status = 1;
 	ft_putstr_fd(_ORANGE2 "exit\n" _END, 2);
 	ft_free_map(*env);
 	ft_lstclear(&lst_command, del_command);
-	// printf ("g = %d\n", g_status);
 	exit(g_status);
 }
