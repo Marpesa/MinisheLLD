913c20077f28ab0aed8830470e145b85490f84a7
Gurvan LE MINIER <gle-mini@e3r4p21.clusters.42paris.fr>
2023-03-12 06:03:53 +0100

    fix ft_str_merge_secure in libft

--- libft/ft_str_merge_secure.c
+++ libft/ft_str_merge_secure.c
@@ -6,7 +6,7 @@
 /*   By: gle-mini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 04:14:35 by gle-mini          #+#    #+#             */
-/*   Updated: 2023/01/13 18:02:23 by gle-mini         ###   ########.fr       */
+/*   Updated: 2023/03/12 05:56:40 by gle-mini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ char	*ft_str_merge_secure(char *str1, char *str2)
 	{
 		ft_memcpy(result, str1, ft_strlen_secure(str1));
 		free(str1);
+		str1 = NULL;
 	}
 	if (str2 != NULL)
 	{
 		ft_memcpy(result, str2, ft_strlen_secure(str2));
 		free(str2);
+		str2 = NULL;
 	}
 	result[ft_strlen_secure(str1) + ft_strlen_secure(str2) + 1] = '\0';
 	return (result);
