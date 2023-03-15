a2441912c5ff53c958a7c4045c8ac606317bed0d
Louise MERY <lmery@e1r1p7.clusters.42paris.fr>
2023-03-15 11:25:58 +0100

    lld easter egg with interactive link

--- exec/builtin_utils_2.c
+++ exec/builtin_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: lmery <lmery@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 20:03:26 by lmery             #+#    #+#             */
-/*   Updated: 2023/03/14 20:54:37 by gle-mini         ###   ########.fr       */
+/*   Updated: 2023/03/15 11:24:32 by lmery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	builtin_lld(char **cmd)
 	else
 	{
 		ft_putstr_fd(_ORANGE2 \
-		"\n----------------------------------------------------\
-		-------\n", STDOUT_FILENO);
+		"\n-----------------------------------------------------------\n", \
+		STDOUT_FILENO);
 		ft_putstr_fd(_BLUE_LLD _BOLD \
 		"\n\t***   You have found an easter egg !   ***\n\n", STDOUT_FILENO);
 		ft_putstr_fd(_END _ORANGE \
@@ -29,7 +29,7 @@ int	builtin_lld(char **cmd)
 		ft_putstr_fd(_BOLD \
 		"   \t      Send us a message on Discord\n\n", STDOUT_FILENO);
 		ft_putstr_fd(_END \
-		"  \t      >>>> s.bde42.fr/discord <<<<\n", STDOUT_FILENO);
+		"  \t  >>>> https://s.bde42.fr/discord <<<<\n", STDOUT_FILENO);
 		ft_putstr_fd(_ORANGE2 \
 		"\n-----------------------------------------------------------\n\n" \
 		_END, STDOUT_FILENO);
